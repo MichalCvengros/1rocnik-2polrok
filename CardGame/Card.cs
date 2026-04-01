0ef13fdcfdcc1b52cf6c3c9ec70bc3ec9c3e3e12
mcvengros66@gmail.com <mcvengros66@gmail.com>
2026-04-01 11:45:44 +0200

    Card Game

---
+++ CardGame/Card.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace CardGame
+{
+    internal class Card : Grid
+    {
+        private TextBlock ManaTextBlock;
+        private TextBlock HPTextBlock;
+        private TextBlock AttackTextBlock;
+
+        public Card(double height, int mana, int hp, int damage)
+        {
+            ManaTextBlock = new TextBlock()
+            {
+                Text = mana.ToString() + " ",
+                HorizontalAlignment = HorizontalAlignment.Left,
+                VerticalAlignment = VerticalAlignment.Top,
+                Background = Brushes.DarkBlue,
+                Foreground = Brushes.Black,
+                FontSize = 16,
+
+                Margin = new Thickness(2)
+            };
+            HPTextBlock = new TextBlock()
+            {
+                Text = " " + hp.ToString(),
+                HorizontalAlignment = HorizontalAlignment.Right,
+                VerticalAlignment = VerticalAlignment.Top,
+                Background = Brushes.DarkGreen,
+                Foreground = Brushes.Black,
+                FontSize = 16,
+                Margin = new Thickness(2)
+            };
+            AttackTextBlock = new TextBlock()
+            {
+                Text = " " + damage.ToString() + " ",
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Top,
+                Background = Brushes.DarkRed,
+                Foreground = Brushes.Black,
+                FontSize = 16,
+                Margin = new Thickness(2),
+
+            };
+
+            Width = 80;
+            Height = height;
+            Background = Brushes.Red;
+            Margin = new Thickness(2);
+
+            Children.Add(ManaTextBlock);
+            Children.Add(HPTextBlock);
+            Children.Add(AttackTextBlock);
+
+            MouseDown += OnCardClicked;
+        }
+        private void OnCardClicked(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is Card card)
+            {
+
+            }
+        }
+    }
+}
---
+++ CardGame/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CardGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Stack<Card> deck = [];
+        public MainWindow()
+        {
+            InitializeComponent();
+            P1_Cards.Height = 100;
+            P1_Cards.Orientation = Orientation.Horizontal;
+
+            deck.Push(new Card(P1_Cards.Height, 5, 7, 3));
+            deck.Push(new Card(P1_Cards.Height, 2, 2, 2));
+            deck.Push(new Card(P1_Cards.Height, 5, 5, 5));
+
+            /*Card card1 = new Card(P1_Cards.Height, 5, 7, 3);
+            Card card2 = new Card(P1_Cards.Height, 7, 6, 2);
+            P1_Cards.Children.Add(card1);
+            P1_Cards.Children.Add(card2);*/
+        }
+
+        private void NextTurn_Click(object sender, RoutedEventArgs e)
+        {
+            if(deck.Count > 0)
+            {
+                Card dreamcard = deck.Pop();
+                P1_Cards.Children.Add(dreamcard);
+            }
+        }
+    }
+}
