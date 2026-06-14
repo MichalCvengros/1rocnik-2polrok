eb356443132c6c336b6edfd85fcb595442ebab3d
MichalCvengros <mcvengros66@gmail.com>
2026-06-14 21:21:25 +0200

    fyjtgyu

---
+++ GunShop/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GunShop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+        }
+
+        private void ShopBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Shop shop = new Shop(this);
+            shop.Show();
+
+            foreach (var item in GunList.Items)
+            {
+                shop.ShopList.Items.Add(item.ToString());
+            }
+        }
+
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+            GunList.Items.Add(GunText.Text);
+        }
+    }
+}
---
+++ GunShop/Shop.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GunShop
+{
+    /// <summary>
+    /// Interaction logic for Shop.xaml
+    /// </summary>
+    public partial class Shop : Window
+    {
+        private MainWindow mainWindow;
+
+        public Shop(MainWindow mainWindow)
+        {
+            InitializeComponent();
+            this.mainWindow = mainWindow;
+        }
+
+        private void CartBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Cart cart = new Cart(this);
+            cart.Show();
+
+            cart.CartList.Items.Add(ShopList.SelectedItem.ToString());
+        }
+        private void BackBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
